10790650ad1f8d6dfb52e78fb5d6ac0b2e19ae3c
tobias.schlueter <tobias.schlueter@nitrobox.com>
2020-11-14 18:04:59 +0100

    change project structure

--- src/app.ts
+++ src/app.ts
@@ -3,21 +3,24 @@ import mongoose from "mongoose"
 import cors from "cors"
 import todoRoutes from "./routes"
 import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app: Express = express()
 
-const PORT: string | number = 80
+const PORT: string | number = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cors())
 app.use(todoRoutes)
 
-const uri: string = `mongodb+srv://gastro-order:9hhX1zSOBpIu3FI2@gastro-order.y5kfk.mongodb.net/gastro-order?retryWrites=true&w=majority`
+const url = process.env.MONGO_URL ||"";
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.set("useFindAndModify", false)
 
 mongoose
-  .connect(uri, options)
+  .connect(url, options)
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server running on http://localhost:${PORT}`)
