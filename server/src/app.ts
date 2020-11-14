2a8498e092568a9399125d8cf85f40a11d906597
tobias.schlueter <tobias.schlueter@nitrobox.com>
2020-11-14 11:41:25 +0100

    initial commit

---
+++ server/src/app.ts
@@ -0,0 +1,27 @@
+import express, { Express } from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import todoRoutes from "./routes"
+
+const app: Express = express()
+
+const PORT: string | number = process.env.PORT || 4000
+
+app.use(express.json())
+app.use(cors())
+app.use(todoRoutes)
+
+const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@gastro-order.y5kfk.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.set("useFindAndModify", false)
+
+mongoose
+  .connect(uri, options)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    )
+  )
+  .catch(error => {
+    throw error
+  })
